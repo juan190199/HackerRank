4691a5ea2c343f2f8f1a3b97aaa676b841eb4d5e
Juan Da Silva <juanmanueldasilvatome@gmail.com>
2019-11-12 00:45:35 +0100

    93 percent HackerRank C++ challenges

---
+++ HackerRank_31/HackerRank_31/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  HackerRank_31
+//
+//  Created by Juan on 10.11.19.
+//  Copyright © 2019 Juan. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    scanf("%i %li %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%i\n%li\n%c\n%f\n%lf", a, b, c, d, e);
+    
+    return 0;
+}
