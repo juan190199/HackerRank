54229fd1627a54bc7d82aed246bddffc8e46dedf
Juan Da Silva <juanmanueldasilvatome@gmail.com>
2019-11-17 11:24:13 +0100

    98 percent HackerRank

--- HackerRank_32/HackerRank_32/HackerRank_32/main.cpp
+++ HackerRank_32/HackerRank_32/HackerRank_32/main.cpp
@@ -7,51 +7,84 @@
 //
 
 #include <iostream>
+#include <vector>
 #include <map>
-
+#include <strstream>
+#include <string>
 
 using namespace std;
 
 
-map<string, string> tagMap;
-
-void createMap(int &N, string pretag)
+int main()
 {
-    if(!N)
-        return;
+    int n, q, i;
+    cin >> n >> q;
+    string temp;
+    vector<string> hrml;
+    vector<string> queries;
     
-    string line;
-    string tag;
-    string attr;
-    string value;
-    
-    getline(cin, line);
-    
-    int i = 1;
-    if(line[i] == '/') // Closing tag
+    cin.ignore(); // Used for clear the buffer while taking next line input
+
+    for(i = 0; i < n; i++)
     {
-        while(line[i] != '>')
-            i++;
-        if(pretag.size() > (i-2))
-            tag = pretag.substr()
-            
+        getline(cin,temp);
+        hrml.push_back(temp);
     }
-    
-    
-    
-}
+    for(i = 0; i < q; i++)
+    {
+        getline(cin,temp);
+        queries.push_back(temp);
+    }
 
+    map<string, string> mp;
+    vector<string> tag;
 
-int main(int argc, const char * argv[])
-{
-    int N, Q;
-    cin >> N >> Q;
-    
-    
-    
-    
-    
-   
-    
+    for(i = 0; i < n; i++)
+    {
+        temp = hrml[i];
+        temp.erase(remove(temp.begin(), temp.end(), '\"' ), temp.end()); // Remove "
+        temp.erase(remove(temp.begin(), temp.end(), '>' ), temp.end()); // Remove >
+
+        if(temp.substr(0,2) == "</") // Returns a substring [pos, pos+count)
+        {
+            tag.pop_back();
+        }
+        else
+        {
+            strstream ss;
+            ss.str();
+            ss << temp;
+            string t1 , p1, v1;
+            char ch;
+            ss >> ch >> t1 >> p1 >> ch >> v1; // ch == < and =, t1 == tag, p1 == attribute name, v1 == value
+            string temp1 = "";
+
+            if(tag.size() > 0)
+            {
+                temp1 = *tag.rbegin(); // Returns an iterator to the reverse-beginning of the given container c or array array.
+                temp1 = temp1 + "." + t1;
+            }
+            else
+                temp1 = t1;
+
+            tag.push_back(temp1);
+            mp[*tag.rbegin() + "~" + p1] = v1;
+
+            while(ss)
+            {
+                ss >> p1 >> ch >> v1;
+                mp[*tag.rbegin() + "~" + p1] = v1;
+            }
+        }
+
+    }
+
+    for(i = 0;i < q; i++)
+    {
+        if (mp.find(queries[i]) == mp.end())
+            cout << "Not Found!\n";
+        else
+            cout << mp[queries[i]] << endl;
+    }
     return 0;
 }
